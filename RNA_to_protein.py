d71b2720c85ce081e3b2e58f4f1494381ed14b46
Alisa Gorislav <61160686+alyferryhalo@users.noreply.github.com>
2021-05-10 14:38:50 +0500

    Update RNA_to_protein.py

--- RNA_to_protein.py
+++ RNA_to_protein.py
@@ -1,5 +1,6 @@
 # The 20 commonly occurring amino acids are abbreviated by using 20 letters from the English alphabet. 
-# Protein strings are constructed from these 20 symbols. Henceforth, the term genetic string will incorporate protein strings along with DNA strings and RNA strings.
+# Protein strings are constructed from these 20 symbols. Henceforth, the term genetic string 
+# will incorporate protein strings along with DNA strings and RNA strings.
 
 # Given: An RNA string s corresponding to a strand of mRNA (of length at most 10 kbp).
 # Return: The protein string encoded by s.
